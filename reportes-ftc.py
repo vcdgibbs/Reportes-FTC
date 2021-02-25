f340b3ef709007cf0707b0bc435d470660caf204
vcdgibbs <vcdgibbs@gmail.com>
2021-02-25 10:01:28 -0300

    mas codigo

---
+++ reportes-ftc.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python3
+
+# número de comuna:
+#2345678901234567890123456789012345678901234567890123456789012345678901234567890
+helpMsg='''
+reportes-ftc.py -params valor opcion
+
+Descripción:  Armar el resporte requerido de los proyectos y uso de recursos en 
+              un archivo excel (.xlsx) o un archivo .csv
+
+Parameters:
+   -prism <ip>      : Prism Central IP address.
+   -user <username> : username who will execute the commands. (optional)
+   -password <pwd>  : username's password (optional)
+
+
+Options:
+   archivo : nombre del aarchivo excel, no es necesario la extensión .xlsx, si no 
+   está en el nombre, se agregará automáticamente.
+   help    : display this text as help.
+
+Link:  https://github.com/vcdgibbs/Reportes-FTC
+
+Notes:
+
+Author: Victor D'Gibbs (victor.dgibbs@nutanix.com)
+Revision:  February 25, 2021
+
+To do:
+- todo por ahora
+
+'''
+import requests
+import json
+import urllib3
+import sys
+import stdiomask
+from utiles import *
+
+'''
+Se desabilita el "Warning" de conexion insegura. Esto lo dejo asi solo por estar en un ambiente de desarrrollo.
+En un ambiente productivo esto no deberia estar.
+'''
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# let's construct a DICT class from the arguments:
+pars = params2dict(sys.argv)
+
+# Revision de parametros:
+
+if "error" in pars:
+    printError(pars['error'])
+    exit(1)
+
+if "help" in pars:
+    print(helpMsg)
+    exit(0)
+
+if "prism" in pars:
+    if valid_ip(pars['prism']):
+        PC_IP=pars["prism"]
+        try:
+            t_url="https://"+PC_IP+":9440/console/#login"
+            r1 = requests.get(t_url, verify=False)
+        except:
+            printError("Trying to go to "+ t_url + ", but it is unreachable, is the Prism Central IP address " + PC_IP + " Correct?")
+            exit(1)
+
+    else:
+        print(Colores.fg.red+"[Error] Please provide a valid IP address." + Colores.reset)
+        exit(1)
+else:
+    print(Colores.fg.red+"[Error] Prism Central address not provided." + Colores.reset)
+    exit(1)
+
+if "user" in pars:
+    User = pars["user"]
+else:
+    User = input("User: ")
+
+if "password" in pars:
+    Password = pars["password"]
+else:
+    Password = stdiomask.getpass()
+
+# Variables
+API_Server_End_Point="https://" + PC_IP + ":9440/api/nutanix/v3/"
+PrismCreds = {
+    "User" : User,
+    "Password" : Password
+    }
+
+
+
+###############################################################
+# 1. Rescatar la información de los proyectos
+###############################################################
+
+comURL=API_Server_End_Point + "projects/list"
+pl={"kind":"project"}
+
+try:
+    info_proyectos = Prism_API_Call("POST",comURL,PrismCreds,pl)
+    print(json.dumps(info_proyectos, indent=2))
+
+except:
+        printError("Something get wrong, finishing")
+        exit(1)
+
+
+###############################################################
+# 2. Crear tabla (en variable dict) con la información
+###############################################################
+# tabla_proyectos {
+#     "tenant-1" : {"nombre":"<nombre del proyecto",
+#                   "owner": "<información del dueño del proyecto>",   // usaré la descripción
+#                   "vcpu_uso": "<vcpu usado>",
+#                   "vcpu_total": "<vcpu asignado al proyecto>",    // es posible que no tenga cuotas
+#                   "mem_uso": "<memoria usada>>",
+#                   "mem_total":"<memoria asignada al proyecto>",   // es posible que no tenga cuotas
+#                   "hdd_uso": "<storage usado>",
+#                   "hdd_total": "<storage asognado al proyecto>"   // es posible que no tenga cutoas
+#                   }
+# }
+i=0
+t = {}
+tabla_proyectos = {}
+while i < int(info_proyectos["metadata"]["length"]):
+    print(info_proyectos["entities"][i]["status"]["name"])
+    t["nombre"]=info_proyectos["entities"][i]["status"]["name"]
+    t["owner"]=info_proyectos["entities"][i]["status"]["description"]
+    t["vcpu_uso"]=0
+
+    print(info_proyectos["entities"][i]["status"]["resources"]["resource_domain"])
+    print(len(info_proyectos["entities"][i]["status"]["resources"]["resource_domain"]))
+
+
+    i+=1 
+
+
+#  with open(pars['sourcecsv']) as csv_file:
+#         csv_reader = csv.reader(csv_file, delimiter=',')
+#         line_count = 0
+#         for row in csv_reader:
+#             if line_count == 0:
+#                 #print(f'Column names are {", ".join(row)}')
+#                 # Init some vars
+#                 myVarItem={}
+#                 myListVMs={}
+#                 line_count += 1
+#             else:
+#                 # Make sure row has 3 values
+#                 if len(row) != 3:
+#                     continue
+#                 nn=row[0].strip()
+#                 cc=row[1].strip()
+#                 vv=row[2].strip()
+#                 myVarItem = { 
+#                     "vm_name" : nn.replace(" ", "_"),
+#                     "category_name" : cc.replace(" ", "_"),
+#                     "value_name" : vv.replace(" ", "_")
+#                 }
+#                 vmn = "vm" + str(line_count)
+#                 myListVMs[vmn] = myVarItem
+#                 line_count += 1
+# else:
+#     if "vm" in pars and "category" in pars and "value" in pars:
+#         # código para dict de una sola VM
+#         myVarItem = { 
+#             "vm_name" : pars['vm'],
+#             "category_name" : pars['category'],
+#             "value_name" : pars['value']
+#         }
+#         myListVMs = { "vm1" : myVarItem }
+
+# for VMS in myListVMs:
+    
+#     if "add" in pars.keys(): act="add"
+#     else: act="remove"
+#     Category = myListVMs[VMS]["category_name"]
+#     Value = myListVMs[VMS]["value_name"]
---
+++ utiles.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python3
+
+import datetime
+import requests
+
+class Colores:
+    reset='\033[0m'
+    bold='\033[01m'
+    disable='\033[02m'
+    underline='\033[04m'
+    reverse='\033[07m'
+    strikethrough='\033[09m'
+    invisible='\033[08m'
+    class fg:
+        black='\033[30m'
+        red='\033[31m'
+        green='\033[32m'
+        orange='\033[33m'
+        blue='\033[34m'
+        purple='\033[35m'
+        cyan='\033[36m'
+        lightgrey='\033[37m'
+        darkgrey='\033[90m'
+        lightred='\033[91m'
+        lightgreen='\033[92m'
+        yellow='\033[93m'
+        lightblue='\033[94m'
+        pink='\033[95m'
+        lightcyan='\033[96m'
+    class bg:
+        black='\033[40m'
+        red='\033[41m'
+        green='\033[42m'
+        orange='\033[43m'
+        blue='\033[44m'
+        purple='\033[45m'
+        cyan='\033[46m'
+        lightgrey='\033[47m'
+
+
+def valid_ip(address):
+    try:
+        host_bytes = address.split('.')
+        valid = [int(b) for b in host_bytes]
+        valid = [b for b in valid if b >= 0 and b<=255]
+        return len(host_bytes) == 4 and len(valid) == 4
+    except:
+        return False
+
+def is_number(n):
+       try:
+           float(n)   # Type-casting the string to `float`.
+                      # If string is not a valid `float`, 
+                      # it'll raise `ValueError` exception
+       except ValueError:
+           return False
+       return True
+
+# Fucntion to get a dict class with parameter
+'''
+The idea is the convert params of:
+
+$ command -param1 val1 -param2 val2 option1 option2
+
+so "-param1 val1" will be a key:value pair as {"param1":"val1"}
+and 
+if "option1" is text, it will we a key:value pair as {"option1":True}
+if "option1" is a number, it will be as {"option1":number} 
+
+Note: All params will be converted to lowercase, values and options will be as entered, for example:
+
+$ comamand -PaRams1 Val1 Option1 oPtiOn2  will take the following form:
+
+returned={'params1'='Val1', 'Option1'=1, 'oPtiOn2'=1}
+'''
+
+def params2dict(args):
+    #print(len(args))
+    # args[0] -> comando mismo
+    # args[1] .. args[n] -> parametros
+
+    p={}
+    # option_number
+    on=1 
+    i=1
+
+    while i<len(args):
+        try:
+            if args[i][0] == "-":
+                # We expect a value for a parameter, so next argument can't begin with '-'
+                if args[i+1][0] == "-":
+                    p={"error":"expected value for " + args[i]}
+                    break
+                #print("está " + args[i][0] + " bien?" +args[i])
+                temp=args[i].strip('-')
+                p[temp.lower()]=args[i+1]
+                i+=2
+            else:
+                if is_number(args[i]):
+                        nopt = "option" + str(on)
+                        # is the number an 'int' or 'float'?  - check whether there is a decimal dot.
+                        if args[i].count(".")==1:
+                            p[nopt]=float(args[i])
+                        else:
+                            p[nopt]=int(args[i])
+                        on+=1
+                else:
+                    p[args[i]]=1
+                i+=1
+            #p[args[i]]=args[i+1]
+        except:
+            p={"error":"expected value"}
+            i+=1
+            break
+        
+    return p
+
+# Just an easy way...
+def printInfo(msg):
+    fh=str(datetime.datetime.now())
+    print(fh + Colores.fg.green + " [INFO] " + msg + Colores.reset)
+    return 1
+
+def printError(msg):
+    fh=str(datetime.datetime.now())
+    print(fh + Colores.fg.red + " [ERROR] " + msg + Colores.reset)
+    return 1
+
+def printWarning(msg):
+    fh=str(datetime.datetime.now())
+    print(fh + Colores.fg.yellow + " [WARNING] " + msg + Colores.reset)
+    return 1
+
+###################################################################################
+# API Calls to Prism Central
+# https://httpstatuses.com/ is a good page to know the HTTP status codes
+###################################################################################
+
+def Prism_API_Call(Method, URL, Credentials, Payload=None):
+    #printInfo("Making a " + Method + " call to " + URL)
+    headers = {'ContentType':'application/json','Accept':'application/json'}
+    us=Credentials["User"]
+    pw=Credentials["Password"]
+   
+    # GET ##########################################################
+    if Method == "GET":
+        if Payload == None:
+            r = requests.get(URL, auth=(us,pw), verify=False)
+        else:
+            r = requests.get(URL, auth=(us,pw), json=Payload, verify=False)
+
+        if r.status_code!=200 and r.status_code!=202:
+            printError("Response code " + str(r.status_code))
+    
+    # POST #########################################################
+    if Method == "POST":
+        if Payload == None:
+            r = requests.post(URL, auth=(us,pw), verify=False)
+        else:
+            r = requests.post(URL, auth=(us,pw), json=Payload, verify=False)
+
+        if r.status_code!=200 and r.status_code!=202:
+            printError("Response code " + str(r.status_code))
+
+    # PUT ##########################################################
+    if Method == "PUT":
+        if Payload == None:
+            r = requests.put(URL, auth=(us,pw), verify=False)
+        else:
+            r = requests.put(URL, auth=(us,pw), json=Payload, verify=False)
+
+        if r.status_code!=200 and r.status_code!=202:
+            printError("Response code " + str(r.status_code))
+
+    # Python returns a "dict" with the JSON info, so returned value should be json.dumps('d) in order to use it as JSON in another API Call.
+    # if returned JSON hasn't status code, I add it to the Dictionay.
+    ret =  r.json()
+    if "code" not in ret:
+        ret["code"]=r.status_code
+    return ret
